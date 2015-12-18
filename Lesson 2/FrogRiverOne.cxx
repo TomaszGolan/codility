417506a68e3a918c027d99526ce97aa5895c9629
Tomasz Golan <tomasz.golan@gmail.com>
2015-12-18 16:47:09 -0600

    Lesson 2: FrogRiverOne

---
+++ Lesson 2/FrogRiverOne.cxx
@@ -0,0 +1,12 @@
+int solution (int X, vector<int> &A)
+{
+    vector <bool> flag (X, false); // true for already counted leaves
+
+    int goal = (1 + X) * X / 2;    // sum of required leaves positions
+    
+    for (unsigned int i = 0; i < A.size(); i++)     // loop over A
+        if (not flag[A[i]] and (flag[A[i]] = true)) // if leaf not counted: change flag and count leaf
+            if ((goal -= A[i]) == 0) return i;      // return current time if goal accomplished
+    
+    return -1;
+}
