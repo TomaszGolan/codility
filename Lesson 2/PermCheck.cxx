332307f34c73e1ede54511ee344d634ae47838bc
Tomasz Golan <tomasz.golan@gmail.com>
2015-12-18 17:08:01 -0600

    Lesson 2: PermCheck.cxx

---
+++ Lesson 2/PermCheck.cxx
@@ -0,0 +1,12 @@
+#include <algorithm> // for find
+
+int solution (vector<int> &A)
+{
+    vector <int> occurrence (A.size(), 0); // how many times numbers occur
+    
+    for (auto &a : A) if (a < A.size() + 1)     // loop over A and skip numbers greater than N
+        if (++occurrence[a-1] > 1) return false; // return false if a number occurs more than once (not permutation)
+    
+    // if there is a missing number (occurrence == 0) is not a permutation  
+    return find (occurrence.begin(), occurrence.end(), 0) != occurrence.end() ? false : true;
+}
